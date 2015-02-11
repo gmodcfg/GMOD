6d766ed94b7b69b526379159abb4eaa97a87baef
StidOfficial <StidOfficial@users.noreply.github.com>
2015-02-11 21:27:24 +0100

    Create RollScript.lua

---
+++ RollScript.lua
@@ -0,0 +1,26 @@
+if(SERVER) then
+	function roll(ply, text, public)
+		if(string.StartWith(text, "!roll") or string.StartWith(text, "/roll")) then
+			local Return = string.format("Vous avez obtenue %s", math.random( 1, 100))
+			local ReturnAll = string.format("%s a obtenue %s", ply:Name(), math.random( 1, 100))
+
+			ply:PrintMessage(4, Return)
+			ply:ChatPrint(Return)
+	
+			for k, v in pairs(player.GetAll()) do
+				--if (v:Alive() and !v:Name() == ply:Name()) then
+				if (v:Alive()) then
+					v:ChatPrint(string.format("Tu est a %s de distance de %s par rapport a toi qui a %s et %s", ply:GetLocalPos():Distance(v:GetLocalPos()), ply:Name(), v:GetLocalPos(), ply:GetLocalPos()))
+					print(ply:GetLocalPos():Distance(v:GetLocalPos()))
+					if(ply:GetLocalPos():Distance(v:GetLocalPos()) <= 300) then
+						v:ChatPrint(ReturnAll)
+					end
+				end
+			end
+		end
+	end
+	hook.Add("PlayerSay", "roll", roll)
+end
+if(CLIENT) then
+	
+end
